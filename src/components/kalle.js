29878606584cdc61bba0b2ec2ed55a94effa63b1
meg768 <meg768@users.noreply.github.com>
2018-10-14 21:33:32 +0200

    -

--- src/components/kalle.js
+++ src/components/kalle.js
@@ -1,13 +1,30 @@
 import React from 'react';
+import Component from './component.js';
+import PropTypes from "prop-types";
+import classNames from 'classnames';
+import Tag from './tag.js';
 
-function Kalle(props)  {
-    const {children, ...other} = props;
+export default class Kalle extends Component  {
 
-    return (
-        <div {...other}>
-            {children}
-        </div>
-    );
-}
 
-export default Kalle;
+    static propTypes = {
+        fluid : PropTypes.bool
+    };
+
+    static defaultProps = {
+        fluid: false
+    }
+
+    render() {
+        var {className, fluid, ...props} = this.props;
+
+        className = classNames(className, {'container' : true});
+        className = classNames(className, {'container-fluid' : fluid});
+
+        return (
+            <div className={className} {...props}>
+                {this.props.children}
+            </div>
+        );
+    }
+};
