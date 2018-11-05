1e967e650659dded75c93440faf7a98f0df20b50
meg768 <meg768@users.noreply.github.com>
2018-11-05 17:52:42 +0100

    -

--- src/collapse/index.js
+++ src/collapse/index.js
@@ -5,6 +5,10 @@ import classNames from 'classnames';
 import Transition from 'react-transition-group/Transition';
 import {isArray} from '../utils';
 
+function debug() {
+   console.log.apply(null, arguments);
+}
+
 
 export default class Collapse extends React.Component  {
 
@@ -13,64 +17,49 @@ export default class Collapse extends React.Component  {
 
         this.state = {};
         this.state.height = null;
-        this.state.opacity = 0;
+        this.state.opacity = null;
         
         this.onEntering = this.onEntering.bind(this);
-        this.onEntered = this.onEntered.bind(this);
-        this.onExiting = this.onExiting.bind(this);
-        this.onExited = this.onExited.bind(this);
-        this.onExit = this.onExit.bind(this);
-        this.onEnter = this.onEnter.bind(this);
+        this.onEntered  = this.onEntered.bind(this);
+        this.onExiting  = this.onExiting.bind(this);
+        this.onExited   = this.onExited.bind(this);
+        this.onExit     = this.onExit.bind(this);
+        this.onEnter    = this.onEnter.bind(this);
     }
 
     onEnter(node) {
-        this.setState({height:0, opactity:0});
-        console.log('onEnter state');
+        debug('onEnter state');
+        this.setState({height:0, opacity:0});
     }
 
-
-
-    
     onEntering(node, isAppearing) {
+        debug('onEntering');
         this.setState({height:node.scrollHeight, opacity:1});
-        console.log('onEntering');
     }
 
     onEntered(node, isAppearing) {
-        console.log('onEntered');
+        debug('onEntered');
     }
 
     onExit(node) {
-        console.log('onExit');
+        debug('onExit');
     }
 
     onExiting(node) {
-        // getting this variable triggers a reflow
+        debug('onExiting');
+
+        // Getting this variable triggers a reflow
         const _unused = node.offsetHeight; // eslint-disable-line no-unused-vars
+
         this.setState({height:0, opacity:0});
-        console.log('onExiting');
     }
 
     onExited(node) {
-        this.setState({height:null, opacity:0});
-        console.log('onExited');
-    }
-    /*
-    onEntering(node, isAppearing) {
-        this.setState({style:{height:node.scrollHeight}});
+        debug('onExited');
+        this.setState({height:null, opacity:null});
     }
 
-    onEntered(node, isAppearing) {
-    }
-
-    onExiting(node) {
-        this.setState({style:{height:0}});
-    }
-
-    onExited(node) {
-        this.setState({style:{}});
-    }
-*/
+    
     render() {
         var {show, fade, timeout, children, ...other} = this.props;
 
@@ -84,14 +73,9 @@ export default class Collapse extends React.Component  {
      
                     var className = child.props.className;
                     var style = child.props.style || {};
-                    
-//                    if (this.state.height != null) {
-  //                      style = {...style, ...{height:this.state.height}};
-    //                }
-                    
+
                     switch (state) {
                         case 'entering': {                
-                            //className = classNames(className, 'collapsing');                
                             style = {...style, ...{position:'ralative', overflow:'hidden'}};
                             style = {...style, ...{transition:`height ${timeout / 1000}s ease, opacity ${timeout / 1000}s ease`}};
                             style = {...style, ...{height:this.state.height, opacity:this.state.opacity}};
@@ -100,11 +84,9 @@ export default class Collapse extends React.Component  {
                         };
 
                         case 'entered': {
-                            //className = classNames(className, 'collapse show');                
                             break;
                         };
                         case 'exiting': {
-                            //className = classNames(className, 'collapsing');                
                             style = {...style, ...{position:'ralative', overflow:'hidden'}};
                             style = {...style, ...{transition:`height ${timeout / 1000}s ease, opacity ${timeout / 1000}s ease`}};
                             style = {...style, ...{height:this.state.height, opacity:this.state.opacity}};
@@ -112,22 +94,12 @@ export default class Collapse extends React.Component  {
                         };
 
                         case 'exited': {
-                            //className = classNames(className, 'collapse');                
                             style = {...style, ...{display:'none'}};
                             break;
                         };
                     }
  
-                    console.log('style at', state, style);
-
-                    /*
-
-                    className = classNames(className, {'collapse show' : state == 'entered'});
-                    className = classNames(className, {'collapsing'    : state == 'entering'});
-                    className = classNames(className, {'collapsing'    : state == 'exiting'});
-                    className = classNames(className, {'collapse'      : state == 'exited'});
-                    className = classNames(className, {'fade'          : (state == 'entering' || state == 'exiting') && this.props.fade});
-                    */
+                    debug('Style for state', state, style);
 
                     return React.cloneElement(child, {className: className, style: style, ...other});
                  }}
@@ -143,7 +115,7 @@ Collapse.propTypes = {
 };
 
 Collapse.defaultProps = {
-    timeout: 350,
+    timeout: 300,
     fade: false,
     show: false
 };
