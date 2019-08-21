d237730d2e9c28c1fd833b1b48abb66c7497bfaa
meg768 <meg768@users.noreply.github.com>
2019-08-21 18:38:57 +0200

    -

--- src/form/index.js
+++ src/form/index.js
@@ -92,7 +92,6 @@ Form.Input = class extends React.Component {
         className = classNames(className, {'form-control':true});
         className = classNames(className, {'form-control-sm':size=='sm'});
         className = classNames(className, {'form-control-lg':size=='lg'});
-        className = classNames(className, {'form-control-lg':true});
         className = classNames(className, {'form-control-plaintext':plainText});
     
         return (
