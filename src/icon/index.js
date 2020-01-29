75a8425ef8577c089634e81d493a4e1592ade494
meg768 <meg768@users.noreply.github.com>
2020-01-29 22:08:26 +0100

    -

--- src/icon/index.js
+++ src/icon/index.js
@@ -11,6 +11,36 @@ export default function Icon(props) {
 
     if (icon == undefined) {
         tag = 'div';
+        props = {};
+        children = 'No icon specified';
+    }
+
+    else if (isString(icon)) {
+        tag = 'img';
+        props = {src:icon, style:{width:width, height:height, ...style}, ...props};
+    }
+
+    else {
+        tag = icon;
+        props = {width:width, height:height, style:style, ...props};
+    }
+
+    return (
+        <Tag tag={tag} {...props}>
+            {children}
+        </Tag>
+    );
+};
+
+
+/*
+
+export default function Icon(props) {
+
+    var {tag, icon, style, width, height, children, ...props} = props;
+
+    if (icon == undefined) {
+        tag = 'div';
         children = 'No icon specified';    
     }
     else if (isString(icon)) {
@@ -27,6 +57,8 @@ export default function Icon(props) {
         </Tag>
     );
 };
+*/
+
 
 Icon.defaultProps = {
 };
