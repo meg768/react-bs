c5d2a7abcc408463bbade0dde93109ecda699a52
meg768 <meg768@users.noreply.github.com>
2020-02-02 18:13:28 +0100

    -

--- src/icon/index.js
+++ src/icon/index.js
@@ -5,19 +5,22 @@ export default function Icon(props) {
 
     var {tag, src, icon, style, width, height, children, ...props} = props;
 
-    if (icon && isFunction(icon.default)) {
-        icon = icon.default;
+	if (icon && !src)
+		src = icon;
+
+    if (src && isFunction(src.default)) {
+        src = src.default;
     }
         
     if (isString(src)) {
-        tag = 'img', props = {src:src, style:{width:width, height:height, ...style}, ...props};
+		props = {tag:'img', src:src, style:{width:width, height:height, ...style}, ...props};
     }
-    else if (icon) {
-        tag = icon, props = {width:width, height:height, style:style, ...props};
-    }
+    else  {
+		props = {tag:src, width:width, height:height, style:style, ...props};
+	}
 
     return (
-        <Tag tag={tag} {...props}>
+        <Tag {...props}>
             {children}
         </Tag>
     );
