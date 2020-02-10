9cbcac59c1e28abd18afa55520d500c681220ab8
meg768 <meg768@users.noreply.github.com>
2020-02-10 18:49:39 +0100

    -

--- src/form/index.js
+++ src/form/index.js
@@ -28,8 +28,27 @@ Form.Group = function(props)  {
 
     var {row, className, ...props} = props;
 
+    // Backward compatability
+    var {xs, sm, md, lg, xl, ...props} = props;
+
     className = classNames(className, {'row': row});
     className = classNames(className, {'form-group': true});
+
+    function fix(name, prop) {
+        if (isObject(prop)) {
+            className = classNames(className, prop.offset ? `offset-${name}-${prop.offset}` : undefined);
+            className = classNames(className, prop.width  ? `col-${name}-${prop.width}`     : undefined);
+        }
+        else if (prop != undefined) {
+            className = classNames(className, `col-${name}-${prop}`);
+        }
+    }
+
+    fix('xs', xs);
+    fix('sm', sm);
+    fix('md', md);
+    fix('lg', lg);
+    fix('xl', xl);
 
     return (
         <Tag className={className}  {...props}>
