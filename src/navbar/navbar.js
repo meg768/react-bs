ca16dca19104dffd2c16ff7fa3d8daaae7984e5f
meg768 <meg768@users.noreply.github.com>
2020-02-22 18:42:33 +0100

    -

--- src/navbar/navbar.js
+++ src/navbar/navbar.js
@@ -25,7 +25,8 @@ export default function NavBar(props) {
 
 NavBar.propTypes = {
     /** Default tag */
-    tag: PropTypes.string,
+	tag: PropTypes.string,
+	/** Color */
     color: PropTypes.string
 };
 
@@ -38,112 +39,3 @@ NavBar.Brand = Brand;
 NavBar.Collapse = Collapse;
 NavBar.Nav = Nav;
 
-
-/*
-export default class NavBar extends React.Component {
-
-	constructor(props) {
-		super(props);
-
-	}
-	
-	render() {
-			
-	}	
-}
-
-
-NavBar.Toggler = class extends React.Component {
-
-
-	constructor(props) {
-		super(props);
-	}
-
-	render() {
-
-		var {children, className, ...props} = this.props;
-
-		className = classNames(className, {'navbar-toggler': true});
-
-		return(
-			<button className={className} type="button" data-toggle="collapse"  aria-expanded="false" aria-label="Toggle navigation" {...props}>
-				{children || <span className="navbar-toggler-icon"></span>}
-			</button>
-
-		);
-
-	}
-}
-
-NavBar.Nav = class extends React.Component {
-
-
-	constructor(props) {
-		super(props);
-	}
-
-	render() {
-		var {children, className, ...props} = this.props;
-
-		className = classNames(className, {'navbar-nav': true});
-
-		return (
-			<Tag className={className} {...props}>
-				{children}
-			</Tag>
-
-		);
-
-	}
-}
-
-
-NavBar.Collapse = class extends React.Component {
-
-
-	constructor(props) {
-		super(props);
-	}
-
-	render() {
-		var {children, show, className, ...props} = this.props;
-
-		className = classNames(className, {'collapse navbar-collapse': true});
-		className = classNames(className, {show:show});
-
-		return (
-			<Tag className={className} {...props}>
-				{children}
-			</Tag>
-
-		);
-
-	}
-}
-
-
-NavBar.Brand = class extends React.Component {
-
-
-	constructor(props) {
-		super(props);
-	}
-
-	render() {
-		var {tag = 'a', children, className, ...props} = this.props;
-
-		className = classNames(className, {'navbar-brand': true});
-
-		return (
-			<Tag tag={tag} className={className} {...props}>
-				{children}
-			</Tag>
-
-		);
-
-	}
-}
-
-
-*/
